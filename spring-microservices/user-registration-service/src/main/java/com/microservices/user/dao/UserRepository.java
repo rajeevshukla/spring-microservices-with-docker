8b479191e0451331aec85ac08a03ef36c1fbc1ea
rajeevshukla <mail2rajeevshukla@gmail.com>
2018-08-09 22:05:51 -0400

    pom.xml changes.

--- spring-microservices/user-registration-service/src/main/java/com/microservices/user/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.user.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.microservices.user.model.UserDetails;
-
-public interface UserRepository extends CrudRepository<UserDetails, Long> {
-
-}
--- spring-microservices/user-registration-service/src/main/java/com/microservices/user/exception/UserRegistrationException.java
+++
@@ -1,5 +0,0 @@
-package com.microservices.user.exception;
-
-public class UserRegistrationException extends  RuntimeException{
-
-}
