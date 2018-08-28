fdb99084df12c329644bc5c1117b9af56e9f9b3c
rajeevshukla <mail2rajeevshukla@gmail.com>
2018-08-27 23:12:12 -0400

    adding thymleaf email template

--- email-service/src/main/java/com/email/service/sender/UserEmailSenderService.java
+++ email-service/src/main/java/com/email/service/sender/UserEmailSenderService.java
@@ -10,29 +10,39 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @Service
 public class UserEmailSenderService {
 
 	@Autowired
 	JavaMailSender javaMailSender;
-	
+
+	@Autowired 
+	SpringTemplateEngine templateEngine;
+
 	public boolean sendRegistrationEmail(String userJson) {
-		
+
 		boolean isSent = false;
 		try {
 			isSent = false;
 			JSONObject jsonObject = new JSONObject(userJson);
 			System.out.println("Sending registration Email");
-			
+
 			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-			
 			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+			// in case you want to add any attachment to email
+//			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8);
+			//helper.addAttachment("logo.png", new ClassPathResource("memorynotfound-logo.png"));
+
+			Context context = new Context();
+			//context.setVariables(); // to set dynamic variables here. 
+			String html = templateEngine.process("reg-email-template", context);
+            helper.setText(html, true);
 			helper.setTo(jsonObject.getString("email"));
-			helper.setText("<html> <body> <p>Hello "+jsonObject.getString("userName")+"</p> <p> This is test email "
-					+ "from Microservice using spring boot using docker.</p>"
-					+ "<p>Thanks, <br/> Docker Deamon</p>", true);
-	
+
 			helper.setSubject("Thank you for registring on Docker - Microservice Portal");
 			javaMailSender.send(helper.getMimeMessage());
 			isSent =true;
@@ -45,6 +55,6 @@ public class UserEmailSenderService {
 		}
 		return isSent;
 	}
-	
-	
+
+
 }
