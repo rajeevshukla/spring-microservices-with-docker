6636eb1fb3df8b00c24a438e4609db9f90d25858
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2020-08-29 22:26:11 -0500

    Adding spring cloud gateway server project.

---
+++ spring-cloud-gateway-server/src/main/java/com/developervisits/cloud/gateway/SpringCloudGatewayServerApplication.java
@@ -0,0 +1,15 @@
+package com.developervisits.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayServerApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-gateway-server/src/test/java/com/developervisits/cloud/gateway/SpringCloudGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developervisits.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
