0337f5d8b5aa97be6815ec4a65a20043e2989b89
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2020-09-20 16:01:59 -0500

    Update AuthServicesApplication.java

--- auth-service/src/main/java/com/auth/service/AuthServicesApplication.java
+++ auth-service/src/main/java/com/auth/service/AuthServicesApplication.java
@@ -8,16 +8,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AuthServicesApplication implements CommandLineRunner {
-     
-	
-	
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServicesApplication.class, args); 
-		System.out.println(" === Email service is up and running.. ======");
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-	  
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServicesApplication.class, args);
+        System.out.println(" === Email service is up and running.. ======");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
